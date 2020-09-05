c36e3b6b5e917fb995a4a72b2d1118f218597ccd
clapis <clapis@users.noreply.github.com>
2020-09-05 23:12:18 +0200

    list companies

--- pages/companies.vue
+++ pages/companies.vue
@@ -4,23 +4,47 @@
       <template v-slot:main>
         # using (dotnet) {
       </template>
-      <template v-slot:sub>
-        <p>
-          
-        </p>
-      </template>
     </intro>
-    <div class="text-muted text-center">
-      coming soon..
-    </div>
 
+    <div class="companies">
+      <company v-for="company in companies" :key="company.id" :value="company" />
+      <client-only>
+        <infinite-loading @infinite="scroll">
+          <div slot="no-more"></div>
+          <div slot="no-results"></div>
+        </infinite-loading>
+      </client-only>
+    </div>
   </div>
 </template>
 
 <script>
 import Intro from "~/components/intro";
+import Company from "~/components/company";
 
 export default {
-  components: { Intro }
+  components: { 
+    Intro,
+    Company,
+    InfiniteLoading: () => import('vue-infinite-loading')
+  },
+  computed: {
+    companies() {
+      return this.$store.state.companies.list;
+    }
+  },
+  methods: {
+    scroll($state) {
+      this.$store.dispatch("companies/fetch")
+        .then(any => any ? $state.loaded() : $state.complete())
+        .catch(err => $state.error());
+    }
+  }
 }
 </script>
+
+<style lang="scss">
+.company {
+  border-top: 1px solid #efefef;
+}
+</style>
