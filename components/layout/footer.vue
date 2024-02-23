8669700eed76bc38f7630f4e357404567c910614
clapis <clapis@users.noreply.github.com>
2024-02-23 16:21:07 +0100

    demo

--- components/layout/footer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <footer class="mt-5 mb-3">
-      <div class="small text-right text-muted">
-        ø <a href="https://dotnetjobs.hearken.io/" rel="noopener" target="_blank">feedback</a>
-        | made by <a target="_blank" href="https://clapis.github.io/">clapis</a> 
-      </div>
-  </footer>
-</template>
